fb9a7cada7fc9bc42f4c6ddbb06cc044c5c25402
fkuca <fkuca91@gmail.com>
2022-07-02 22:50:18 +0200

    refactor(utils): return value when currentLevel equal 0 in calculateBonuses util

--- src/modules/skills/utils/calculateBonuses.ts
+++ src/modules/skills/utils/calculateBonuses.ts
@@ -4,7 +4,11 @@ export const calculateBonuses = (skills: SelectedSkill[]) => {
 	let value = 0;
 
 	skills.forEach(({ currentLevel, bonusSheet }) => {
-		value += bonusSheet[currentLevel - 1].value;
+		if (currentLevel === 0) {
+			return value;
+		}
+
+		return (value += bonusSheet[currentLevel - 1].value);
 	});
 
 	return value;
