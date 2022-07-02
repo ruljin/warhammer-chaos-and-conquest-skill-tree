5456eeed45b9c84445f70eb49acf2f41d0bfc7e4
fkuca <fkuca91@gmail.com>
2022-07-02 09:29:56 +0200

    refactor(models): change Skill model

--- src/modules/skills/models/Skill.ts
+++ src/modules/skills/models/Skill.ts
@@ -1,10 +1,20 @@
 import { Bonus } from "./Bonus";
 
+export type SelectedSkill = Pick<
+	Skill,
+	"name" | "bonusType" | "bonusSheet" | "isPercentage"
+> & {
+	currentLevel: number;
+};
+
 export type Skill = {
 	name: string;
 	description: string;
+	icon: string;
 	maxLevel: number;
 	bonusType: string;
 	bonusSheet: Bonus[];
 	isPercentage: boolean;
+	childrenSkills?: Skill[];
+	requirement?: SelectedSkill;
 };
